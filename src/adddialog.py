a260c32f59c1dae6bb77c1ab9899ea9cd50f1c34
Og B. Maciel <ogmaciel@src.gnome.org>
2007-05-10 00:38:09 +0000

    Added most widgets to adddialog class and added method to instantiate it when clicking the "New" menu entry.
    svn path=/branches/billreminder-0.3/; revision=237

--- src/adddialog.py
+++ src/adddialog.py
@@ -11,5 +11,53 @@ class AddDialog(gtk.Dialog):
     def __init__(self, title=None, parent=None):
         gtk.Dialog.__init__(self, title=title, parent=parent)
 
+        # Set up the UI
+        self._initialize_dialog_widgets()
+
     def _initialize_dialog_widgets(self):
-        pass
+        self.topcontainer = gtk.HBox(homogeneous=False, spacing=0)
+        self.calbox = gtk.VBox(homogeneous=False, spacing=0)
+        self.fieldbox = gtk.VBox(homogeneous=False, spacing=0)
+
+        # Add calendar and label
+        self.callabel = gtk.Label()
+        self.callabel.set_markup("<b>Due Date:</b>")
+        self.calendar = gtk.Calendar()
+        ## Pack it all up
+        self.calbox.pack_start(self.callabel, expand=True, fill=True, padding=0)
+        self.calbox.pack_start(self.calendar, expand=True, fill=True, padding=0)
+
+        # Fields
+        self.table = gtk.Table(rows=3, columns=2, homogeneous=False)
+        self.payeelabel = gtk.Label()
+        ## Labels
+        self.payeelabel.set_markup("<b>Payee:</b>")
+        self.amountlabel = gtk.Label()
+        self.amountlabel.set_markup("<b>Amount:</b>")
+        self.noteslabel = gtk.Label()
+        self.noteslabel.set_markup("<b>Note:</b>")
+        ## Fields
+        self.payee = gtk.ComboBoxEntry()
+        self.amount = gtk.Entry()
+        self.notesdock = gtk.ScrolledWindow()
+        self.notesdock .set_shadow_type(gtk.SHADOW_OUT)
+        self.notesdock .set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        self.notes = gtk.TextView()
+        self.notesdock.add_with_viewport(self.notes)
+        ## Pack it all into the table
+        self.table.attach(self.payeelabel, 0, 1, 0, 1)
+        self.table.attach(self.amountlabel, 0, 1, 1, 2)
+        self.table.attach(self.noteslabel, 0, 1, 2, 3)
+        self.table.attach(self.payee, 1, 2, 0, 1)
+        self.table.attach(self.amount, 1, 2, 1, 2)
+        self.table.attach(self.notesdock, 1, 2, 2, 3)
+        ## Pack table
+        self.fieldbox.pack_start(self.table)
+
+        # Everything
+        self.topcontainer.pack_start(self.calbox, expand=True, fill=True, padding=0)
+        self.topcontainer.pack_start(self.fieldbox, expand=True, fill=True, padding=0)
+        self.vbox.pack_start(self.topcontainer, expand=False, fill=False, padding=0)
+
+        # Show all widgets
+        self.show_all()
--- src/maindialog.py
+++ src/maindialog.py
@@ -4,18 +4,19 @@ import pygtk
 pygtk.require('2.0')
 import gtk
 
+# Import widgets modules
 from toolbar import Toolbar
 from statusbar import Statusbar
+from viewbill import ViewBill as ViewBill
+
+# Import dialogs modules
+from adddialog import AddDialog
+
+# Import data model modules
 from bill import Bill
-from viewbill import ViewBill as ListView
 from dal import DAL
 
 class MainDialog:
-
-    # close the window and quit
-    def on_delete_event(self, widget, event, data=None):
-        gtk.main_quit()
-        return False
 
     def __init__(self):
         # Create a new window
@@ -27,12 +28,13 @@ class MainDialog:
 
         self.box = gtk.VBox(homogeneous=False, spacing=0)
 
-        # ListView
-        self.list = ListView()
+        # ViewBill
+        self.list = ViewBill()
         self.list.connect('cursor_changed', self.on_list_cursor_changed)
 
         # Menubar
         self.menubar = Toolbar()
+        self._populate_menubar()
 
         # ScrolledWindow
         self.scrolledwindow = gtk.ScrolledWindow()
@@ -56,19 +58,20 @@ class MainDialog:
         self.dal = DAL()
         self._populateTreeView(self.dal.get('tblbills', 'paid = 0 ORDER BY dueDate DESC'))
 
-    def on_list_cursor_changed(self, widget):
-        """ 
-            This function will handle the signal sent when a row is selected and 
-            displays the selected record information.
-        """
-        # Get currently selected bill
-        b_id, bill = self._getBill()
+    def _getBill(self):
+        """ Returns a bill object from the current selected record """
+        sel = self.list.get_selection()
+        _model, iteration = sel.get_selected()
 
-        notes = bill.Notes
+        b_id = _model.get_value(iteration, 0)
 
-        # Display the status for the selected row
-        self.statusbar.Notes(notes)
+        records = self.dal.get('tblbills', {'Id': b_id})
+        rec = records[0]
+        
+        # Return bill and id
+        return b_id, Bill(rec)
 
+    # Methods:  UI
     def _populateTreeView(self, records):
         """ Populates the treeview control with the records passed """
 
@@ -92,18 +95,64 @@ class MainDialog:
 
         return formated
 
-    def _getBill(self):
-        """ Returns a bill object from the current selected record """
-        sel = self.list.get_selection()
-        _model, iteration = sel.get_selected()
+    def _populate_menubar(self):
+        self.menubar.add_stock(gtk.STOCK_NEW, "Add a new record", self.on_mnuNew_clicked)
+        self.menubar.add_stock(gtk.STOCK_EDIT, "Edit a record", self.on_mnuEdit_clicked)
+        self.menubar.add_stock(gtk.STOCK_DELETE, "Delete selected record", self.on_mnuDelete_clicked)
+        self.menubar.add_space()
+        self.menubar.add_button(gtk.STOCK_APPLY, "Paid", "Mark as paid", self.on_mnuPaid_clicked)
+        self.menubar.add_button(gtk.STOCK_UNDO, "Not Paid", "Mark as not paid", self.on_mnuNotPaid_clicked)
+        self.menubar.add_space()
+        self.menubar.add_stock(gtk.STOCK_ABOUT, "About the application", self.on_mnuAbout_clicked)
+        self.menubar.add_space()
+        self.menubar.add_stock(gtk.STOCK_CLOSE, "Quit the application", self.on_mnuQuit_clicked)
 
-        b_id = _model.get_value(iteration, 0)
+    def add_bill(self):
+        dialog = AddDialog(title="Add a new record", parent=self.window)
+        ret = dialog.run()
 
-        records = self.dal.get('tblbills', {'Id': b_id})
-        rec = records[0]
-        
-        # Return bill and id
-        return b_id, Bill(rec)
+    # Methods
+    def _quit_application(self):
+        gtk.main_quit()
+        return False
+
+    # Event handlers
+    def on_list_cursor_changed(self, widget):
+        """ 
+            This function will handle the signal sent when a row is selected and 
+            displays the selected record information.
+        """
+        # Get currently selected bill
+        b_id, bill = self._getBill()
+
+        notes = bill.Notes
+
+        # Display the status for the selected row
+        self.statusbar.Notes(notes)
+
+    def on_mnuNew_clicked(self, toolbutton):
+        self.add_bill()
+
+    def on_mnuEdit_clicked(self, toolbutton):
+        pass
+
+    def on_mnuDelete_clicked(self, toolbutton):
+        pass
+
+    def on_mnuPaid_clicked(self, toolbutton):
+        pass
+
+    def on_mnuNotPaid_clicked(self, toolbutton):
+        pass
+
+    def on_mnuAbout_clicked(self, toolbutton):
+        pass
+
+    def on_mnuQuit_clicked(self, toolbutton):
+        self._quit_application()
+
+    def on_delete_event(self, widget, event, data=None):
+        self._quit_application()
 
 def main():
     gtk.main()
