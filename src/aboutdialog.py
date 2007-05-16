bacceb9241353f727c82390935cf6a91514e0ec5
Og B. Maciel <ogmaciel@src.gnome.org>
2007-05-16 01:55:02 +0000

    Changed license to BSD License and added some more code to the aboutdialog.py module.
    svn path=/branches/billreminder-0.3/; revision=241

--- src/aboutdialog.py
+++ src/aboutdialog.py
@@ -3,27 +3,55 @@
 import pygtk
 pygtk.require('2.0')
 import gtk
+import common
 
-class AboutDialog(gtk.Dialog):
+LICENSE = """
+This application is distributed under the BSD 
+Licensing scheme.  An online version of the 
+license can be obtained from 
+http://www.opensource.org/licenses/bsd-license.html.
+
+Copyright (c) 2006, 2007, Og Maciel
+All rights reserved.
+"""
+
+TRANSLATORS = """
+Reinout van Schouwen <reinouts@gnome.org> (nl),
+Daniel Nylander <po@danielnylander.se> (sv),
+Tommy Mikkelsen <tamikkelsen@gmail.com> (no),
+Rolando Espinoza La Fuente <darkrho@gmail.com> (es),
+Gilberto Miralla <xyhthyx@gmail.com> (es)
+"""
+
+AUTHORS = """
+Og Maciel <og.maciel@gmail.com>,
+Laudeci Oliveira <laudeci@gmail.com>,
+Luiz Armesto <luiz.armesto@gmail.com>
+"""
+
+ARTISTS = """
+"""
+
+class AboutDialog(gtk.AboutDialog):
     """
     About dialog class.
     """
     def __init__(self, parent=None):
-        title = "About BillReminder"
-
-        gtk.Dialog.__init__(self, title=title, parent=parent, flags=gtk.DIALOG_MODAL, buttons=(gtk.STOCK_SAVE, gtk.RESPONSE_ACCEPT, gtk.STOCK_CANCEL, gtk.RESPONSE_REJECT))
-
-        if parent:
-            self.set_transient_for(parent)
-            self.set_position(gtk.WIN_POS_CENTER_ON_PARENT)
+        gtk.AboutDialog.__init__(self)
 
         # Set up the UI
         self._initialize_dialog_widgets()
 
     def _initialize_dialog_widgets(self):
-        self.notebook = gtk.Notebook()
-
-        self.vbox.pack_start(self.notebook, expand=False, fill=True, padding=10)
+        self.set_name('BillReminder')
+        self.set_version('0.3')
+        self.set_copyright('Copyright (c) 2006, 2007, Og Maciel')
+        self.set_logo(gtk.gdk.pixbuf_new_from_file(common.APP_HEADER))
+        self.set_translator_credits(TRANSLATORS)
+        self.set_license(LICENSE)
+        self.set_website('http://billreminder.sourceforge.net')
+        self.set_authors(AUTHORS)
 
         # Show all widgets
         self.show_all()
+
