9f21a37366b4937cc5415d851e8993128c8efc6d
x-larmesto <x-larmesto@localhost>
2007-06-21 22:56:42 +0000

    Added DBus support;
    svn path=/branches/billreminder-0.3/; revision=264

--- src/lib/actions.py
+++ src/lib/actions.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import sys
+
 import dal
 import bill
+from lib import common
+from lib.utils import force_string
 from db.billstable import BillsTable
 
 class Actions(object):
@@ -10,7 +14,7 @@ class Actions(object):
     def __init__(self, databaselayer=None):
         if not databaselayer:
             databaselayer = dal.DAL()
-
+        
         self.dal = databaselayer
 
     def get_bills(self, kwargs):
@@ -28,3 +32,7 @@ class Actions(object):
     def delete_bill(self, key):
         """ Delete a record in the database """
         return self.dal.delete(BillsTable, key)
+
+if not '--standalone' in sys.argv and not 'billreminderd' in sys.argv:
+    from lib.dbus_actions import Actions
+
