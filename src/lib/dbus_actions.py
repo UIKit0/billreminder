1afc3be60b0a73ce0dc6407b00626f6822815265
x-larmesto <x-larmesto@localhost>
2007-09-22 19:38:08 +0000

    Now user is asked to restart Daemon and GUI if connection is broken;
    svn path=/branches/billreminder-0.3/; revision=328

--- src/lib/dbus_actions.py
+++ src/lib/dbus_actions.py
@@ -4,11 +4,12 @@
 import dbus
 import dbus.service
 import os
+from subprocess import Popen
 
 import dal
 import bill
 from lib import common
-from lib.utils import force_string
+from lib.utils import force_string, Message
 from db.billstable import BillsTable
 
 class Actions(object):
@@ -18,10 +19,14 @@ class Actions(object):
             session_bus = dbus.SessionBus()
             obj = session_bus.get_object(common.DBUS_INTERFACE, common.DBUS_PATH)
             self.dbus_interface = dbus.Interface(obj, common.DBUS_INTERFACE)
+            pid = os.getpid()
+            print self.dbus_interface.register(pid)
         except dbus.DBusException:
-            pass
-        pid = os.getpid()
-        print self.dbus_interface.register(pid)
+            if Message().ShowErrorQuestion(_("An error occurred while connecting to BillReminder Daemon!\nDo you want to launch it and restart BillReminder?")):
+                Popen('python billreminderd --open-gui', shell=True)
+                raise SystemExit
+            return False
+
 
     def _correct_type(self, record):
         if 'Id' in record.keys():
@@ -36,25 +41,40 @@ class Actions(object):
 
     def get_bills(self, kwargs):
         """ Returns one or more records that meet the criteria passed """
-        if isinstance(kwargs, basestring):
-            records = self.dbus_interface.get_bills_(kwargs)
-        else:
-            records = self.dbus_interface.get_bills(force_string(kwargs))
-
-        for record in records:
-            record = self._correct_type(record)
-        return records
+        try:
+            if isinstance(kwargs, basestring):
+                records = self.dbus_interface.get_bills_(kwargs)
+            else:
+                records = self.dbus_interface.get_bills(force_string(kwargs))
+            for record in records:
+                record = self._correct_type(record)
+            return records
+        except dbus.DBusException:
+            if self.__init__():
+                return self.get_bills(kwargs)
 
     def add_bill(self, kwargs):
         """ Adds a bill to the database """
-        record = self.dbus_interface.add_bill(force_string(kwargs))
-        return self._correct_type(record)
+        try:
+            record = self.dbus_interface.add_bill(force_string(kwargs))
+            return self._correct_type(record)
+        except dbus.DBusException:
+            if self.__init__():
+                return self.get_bills(kwargs)
 
     def edit_bill(self, kwargs):
         """ Edit a record in the database """
-        record = self.dbus_interface.edit_bill(force_string(kwargs))
-        return self._correct_type(record)
+        try:
+            record = self.dbus_interface.edit_bill(force_string(kwargs))
+            return self._correct_type(record)
+        except dbus.DBusException:
+            if self.__init__():
+                return self.get_bills(kwargs)
 
     def delete_bill(self, key):
         """ Delete a record in the database """
-        return self.dbus_interface.delete_bill(key)
+        try:
+            return self.dbus_interface.delete_bill(key)
+        except dbus.DBusException:
+            if self.__init__():
+                return self.get_bills(kwargs)
