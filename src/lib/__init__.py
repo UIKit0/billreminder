7f470737068290268dde2f1f5937fa7087ea6c77
x-larmesto <x-larmesto@localhost>
2007-12-27 21:08:46 +0000

    Added "__all__"
    svn path=/branches/billreminder-0.3/; revision=363

--- src/lib/__init__.py
+++ src/lib/__init__.py
@@ -1,2 +1,5 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+__all__ = ['actions', 'bill', 'bubble', 'common', 'config', 'dal',
+           'dbus_actions', 'dialogs', 'i18n', 'utils']
