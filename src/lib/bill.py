3711533319aecd215e0166a3c765520464ed97cf
x-larmesto <x-larmesto@localhost>
2007-12-27 21:10:17 +0000

    Minor fixes
    svn path=/branches/billreminder-0.3/; revision=365

--- src/lib/bill.py
+++ src/lib/bill.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 __all__ = ['Bill']
@@ -8,7 +8,8 @@ import time
 
 class Bill(object):
 
-    def __init__(self, payee, category='', dueDate='', amountDue='', notes=None, paid=0, id=-1):
+    def __init__(self, payee, category='', dueDate='', amountDue='',
+                 notes=None, paid=0, id=-1):
 
         if isinstance(payee,dict):
             self.__set_id(payee['Id'])
@@ -37,36 +38,42 @@ class Bill(object):
     # Id
     def __get_id (self): return self.__id
     def __set_id (self, value): self.__id  = value
-    Id = property(fget=__get_id, fset=__set_id, doc='Get/Set the Id.')
+    Id = property(fget=__get_id, fset=__set_id,
+                  doc='Get/Set the Id.')
 
     # Category
     def __get_category (self): return self.__category
     def __set_category (self, value): self.__category  = value
-    Category = property(fget=__get_category, fset=__set_category, doc='Get/Set the category.')
+    Category = property(fget=__get_category, fset=__set_category,
+                        doc='Get/Set the category.')
 
     # Payee
     def __get_payee (self): return self.__payee
     def __set_payee (self, value): self.__payee  = value
-    Payee = property(fget=__get_payee, fset=__set_payee, doc='Get/Set the payee.')
+    Payee = property(fget=__get_payee, fset=__set_payee,
+                     doc='Get/Set the payee.')
 
     # DueDate
     def __get_dueDate(self): return self.__dueDate
     def __set_dueDate(self, value):
         #assert isinstance(value, float)
         self.__dueDate = value
-    DueDate = property(fget=__get_dueDate, fset=__set_dueDate, doc='Get/Set the due date.')
+    DueDate = property(fget=__get_dueDate, fset=__set_dueDate,
+                       doc='Get/Set the due date.')
 
     # AmountDue
     def __get_amountDue (self): return self.__amountDue
     def __set_amountDue (self, value):
         #assert isinstance(value, float)
         self.__amountDue = value
-    AmountDue = property(fget=__get_amountDue, fset=__set_amountDue, doc='Get/Set the amount for the bill.')
+    AmountDue = property(fget=__get_amountDue, fset=__set_amountDue,
+                         doc='Get/Set the amount for the bill.')
 
     # Notes
     def __get_notes(self): return self.__notes
     def __set_notes(self, value): self.__notes = value
-    Notes = property(fget=__get_notes, fset=__set_notes, doc='Get/Set notes.')
+    Notes = property(fget=__get_notes, fset=__set_notes,
+                     doc='Get/Set notes.')
 
     # Paid
     def __get_paid(self): return self.__paid
@@ -74,7 +81,8 @@ class Bill(object):
         #assert isinstance(value, int)
         #assert 0 <= value <= 1
         self.__paid = value
-    Paid = property(fget=__get_paid, fset=__set_paid, doc='Get/Set if package was paid.')
+    Paid = property(fget=__get_paid, fset=__set_paid,
+                    doc='Get/Set if package was paid.')
 
     # Dictionary
     def __get_dictionary(self):
@@ -86,7 +94,8 @@ class Bill(object):
             'amountDue': self.__amountDue,
             'notes': self.__notes,
             'paid': self.__paid})
-    Dictionary = property(fget=__get_dictionary, doc='Gets a dictionary representation of bill.')
+    Dictionary = property(fget=__get_dictionary,
+                          doc='Gets a dictionary representation of bill.')
 
     def __repr__(self):
         return self.__payee
