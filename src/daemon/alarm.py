8e7b9331a2deb71bd5c7c1decd8c91a06e7be77f
x-larmesto <x-larmesto@localhost>
2008-01-03 19:33:26 +0000

    Now display individual bill alarm
    svn path=/branches/billreminder-0.3/; revision=404

--- src/daemon/alarm.py
+++ src/daemon/alarm.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 __all__ = ['Alarm']
+# TODO: Clear Code
 
 import datetime
 import time
@@ -27,14 +28,14 @@ class Alarm(object):
         self.tray_hints = {}
         self.parent = parent
         self.tray_hints = {}
-        start_delay = self.parent.config.getint('General', 'delay') * 60000
-        print start_delay
-        timeout_add(start_delay, self.start)
+        self.start()
 
     def start(self):
+        start_delay = self.parent.config.getint('General', 'delay') * 60000
+        print start_delay
         if self.parent.config.getboolean('Alarm', 'show_startup_notification'):
-            self.show_pay_notification()
-        self.verify_due()
+            timeout_add(start_delay, self.show_pay_notification)
+            timeout_add(start_delay + 12000, self.verify_due)
         interval = self.parent.config.getint('Alarm', 'interval') * 1000
         if interval:
             timeout_add(interval, self.timer)
@@ -43,7 +44,7 @@ class Alarm(object):
         notify = NotifyMessage(self.parent)
         notify.title = title
         notify.body = body
-        notify.set_timeout(10)
+        notify.set_timeout(20)
         notify.set_default_action(self.__cb_launch_gui)
         notify.hints = self.tray_hints
         return notify
@@ -68,7 +69,7 @@ class Alarm(object):
             bubble.add_action("close", _("Cancel"), None)
             bubble.show()
 
-        return msg
+        return False
 
     def verify_due(self, sum=0):
         if not self.parent.config.getboolean('Alarm', 'show_due_alarm'):
@@ -91,16 +92,20 @@ class Alarm(object):
         # TODO: use only one dialog for all bills, if use_dialog == True
         for bill in records:
             if sum > 0:
+                # TODO: calculate days
                 timeout_add(i * 12000, self.show_bill_notification, bill,
-                    _("The bill %(bill)s will be due before %(days)d days.") %\
-                      {'bill': "<b>\"%s\"</b>" % bill['payee'], 'days': sum},
+                    _("The bill %(bill)s will be due at %(day)s.") %\
+                      {'bill': "<b>\"%s\"</b>" % bill['payee'],
+                      'day': "<b>\"%s\"</b>" % datetime.datetime.fromtimestamp( \
+                                int(bill['dueDate'])).strftime(_('%Y/%m/%d' \
+                                ).encode('ASCII'))},
                     use_dialog)
             else:
                 timeout_add(i * 12000,
-                            self.show_bill_notification, bill, use_dialog)
+                            self.show_bill_notification, bill, None, use_dialog)
             i += 1
 
-    def show_bill_notification(self, bill=None, msg=None, alert=False):
+    def show_bill_notification(self, bill=None, msg=None, alert=False, timeout=None):
         if msg is None:
             msg = _('The bill %s is due.') % "<b>\"%s\"</b>" % bill['payee']
         if self.parent.actions.get_bills({'Id': bill['Id']})[0]['paid'] == 0:
@@ -117,8 +122,10 @@ class Alarm(object):
                                   self.__cb_mark_as_paid, bill)
                 bubble.add_action("edit", _("Edit"), self.__cb_edit_bill, bill)
                 bubble.add_action("close", _("Cancel"), None)
+                if timeout:
+                    bubble.set_timeout(timeout)
                 bubble.show()
-
+        return False
 
     def __cb_launch_gui(self, *arg):
     # If client is not running, launch it
@@ -149,14 +156,38 @@ class Alarm(object):
                 print str(e)
 
     def timer(self):
-        # TODO: Show custom alarms (get from AlarmsTable)
 
+        interval = self.parent.config.getint('Alarm', 'interval')
         now = datetime.datetime.now()
-        time = "%02d:%02d" % (now.hour, now.minute)
+        alert_hour, alert_minute = self.parent.config.get('Alarm',
+                                     'show_alarm_at_time').split(':')
+        alert_hour = int(alert_hour)
+        alert_minute = int(alert_minute)
+        alert = datetime.datetime(now.year, now.month, now.day,
+                                  alert_hour, alert_minute)
+        now = int(time.mktime(now.timetuple()))
+        alert = int(time.mktime(alert.timetuple()))
         # Alarm for bills wich will be due before n days
         if self.parent.config.getboolean('Alarm', 'show_before_alarm') \
-           and self.parent.config.get('Alarm', 'show_alarm_at_time') == time:
+           and alert >= (now - interval/2) and alert < (now + interval/2):
             days = self.parent.config.getint('Alarm', 'show_alarm_before_days')
             self.verify_due(days)
+
+        records = self.parent.actions.get_bills('alarm >= %d AND ' \
+                        'alarm < %d AND paid = 0' \
+                        % (now - interval/2, now + interval/2))
+
+        i = 0
+        use_dialog = self.parent.config.getboolean('Alarm', 'use_alert_dialog')
+        for bill in records:
+            timeout_add(i * 12000, self.show_bill_notification, bill,
+                    _("The bill %(bill)s will be due at %(day)s.") %\
+                      {'bill': "<b>\"%s\"</b>" % bill['payee'],
+                       'day': "<b>\"%s\"</b>" % datetime.datetime.fromtimestamp( \
+                                int(bill['dueDate'])).strftime(_('%Y/%m/%d' \
+                                ).encode('ASCII'))}, use_dialog, -1)
+
+            i += 1
+        print (now - interval, now), records
 
         return True
