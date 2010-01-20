d1570aa7b8d8faed2f580abac6556ec9deabad02
Toms Bauģis <toms.baugis@gmail.com>
2010-01-20 01:26:06 +0000

    simplified

--- src/gui/widgets/billtree.py
+++ src/gui/widgets/billtree.py
@@ -38,47 +38,37 @@ class BillTree(gtk.TreeView):
     def __init__(self):
         gtk.TreeView.__init__(self)
 
-        self.set_headers_visible(True)
-        self.set_rules_hint(True)
+        self.set_headers_visible(False)
+        self.set_rules_hint(False)
 
         self.store_model = gtk.TreeStore(gtk.gdk.Pixbuf, str, str, str, str, gobject.TYPE_PYOBJECT)
         self.set_model(self.store_model)
 
         # Category Icon
         cat_icon_cell = gtk.CellRendererPixbuf()
-        cat_icon_cell.set_property('xalign', 0.5)
         cat_icon_column = gtk.TreeViewColumn("", cat_icon_cell, pixbuf=CAT_ICON)
-        cat_icon_column.set_resizable(False)
         cat_icon_column.set_clickable(True)
-        cat_icon_column.set_expand(False)
         self.append_column(cat_icon_column)
-
-        # Category Name
-        cat_name_cell = gtk.CellRendererText()
-        cat_name_column = gtk.TreeViewColumn(_("Category"), cat_name_cell, text=CAT_NAME)
-        cat_name_column.set_resizable(True)
-        cat_name_column.set_clickable(True)
-        cat_name_column.set_sort_column_id(CAT_NAME)
-        self.append_column(cat_name_column)
-
-        # Payee Name
-        payee_cell = gtk.CellRendererText()
-        payee_column = gtk.TreeViewColumn(_("Payee"), payee_cell, text=PAYEE)
-        payee_column.set_cell_data_func(payee_cell, self.payee_cell_data_function)
-        self.append_column(payee_column)
 
         # Due Date
         due_date_cell = gtk.CellRendererText()
-        due_date_column = gtk.TreeViewColumn(_("Date"), due_date_cell, text=DUEDATE,)
+        due_date_column = gtk.TreeViewColumn(_("Date"), due_date_cell)
         due_date_column.set_cell_data_func(due_date_cell, self.duedate_cell_data_function)
         self.append_column(due_date_column)
 
+        # Payee Name
+        self.insert_column_with_attributes(-1, _("Name"), gtk.CellRendererText(), text=PAYEE)
+
+
         # Amount Due
         amount_cell = gtk.CellRendererText()
-        amount_label = "%s (%s)" % (_('Amount'), locale.localeconv()['currency_symbol'])
-        amount_column = gtk.TreeViewColumn(amount_label, amount_cell, text=AMOUNT)
+        amount_label = "%s" % (_('Amount'))
+        amount_column = gtk.TreeViewColumn(amount_label, amount_cell)
         amount_column.set_cell_data_func(amount_cell, self.amountdue_cell_data_function)
         self.append_column(amount_column)
+
+
+
 
         # Connect events
         self.connect("row-activated", self._on_row_activated)
@@ -100,7 +90,7 @@ class BillTree(gtk.TreeView):
 
     def duedate_cell_data_function(self, column, cell, model, iter):
         bill = model.get_value(iter, BILL)
-        dueDate = bill.dueDate.strftime(_('%m/%d').encode('ASCII'))
+        dueDate = bill.dueDate.strftime(_('%b %d.').encode('utf-8'))
         cell.set_property('text', dueDate)
         cell.set_property('xalign', 0.5)
         column.set_resizable(False)
@@ -110,7 +100,7 @@ class BillTree(gtk.TreeView):
     def amountdue_cell_data_function(self, column, cell, model, iter):
         bill = model.get_value(iter, BILL)
         amount = utils.float_to_currency(bill.amount)
-        cell.set_property('text', amount)
+        cell.set_property('text', "%s %s" % (amount, locale.localeconv()['currency_symbol']))
         cell.set_property('xalign', 1.0)
         column.set_resizable(True)
         column.set_clickable(True)
@@ -135,7 +125,7 @@ class BillTree(gtk.TreeView):
             if bill.category.color:
                 catColor = bill.category.color
 
-        catIcon = utils.create_pixbuf(color=catColor)
+        catIcon = utils.create_pixbuf(color=catColor, width = 16)
 
         self.store_model.append(parent,
             [catIcon, catName, bill.payee, bill.dueDate, bill.amount, bill]
